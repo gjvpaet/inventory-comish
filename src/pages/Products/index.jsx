684f5a4f68c5770b91ac32e817b7f2311e7a44e5
Gabriel Joshua Paet <gjvpaet@gmail.com>
2018-03-29 18:10:10 +0800

    Added table to products page and created a custom css.

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -12,19 +12,102 @@ import HttpService from '../../services/HttpService';
 const httpService = new HttpService();
 
 class Products extends Component {
+    constructor(props) {
+        super(props);
+
+        ['getProducts'].map(fn => (this[fn] = this[fn].bind(this)));
+    }
+
     async componentDidMount() {
+        this.getProducts();
+    }
+
+    async getProducts() {
         let { fetchProducts } = this.props;
 
         try {
-            let result = await httpService.getAllData('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdqdnBhZXRAZ21haWwuY29tIiwidXNlcklkIjoiNWFhYzViNTdmN2M3ZDY3OTdmZjUwZGI4IiwiaWF0IjoxNTIyMjk3NzI3LCJleHAiOjE1MjIzMDEzMjd9.PVd6ciiIXWrKZoIxUGnlySmr8qpR_yGC3Qq0t2filOE', 'products');
+            let result = await httpService.getAllData(
+                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdqdnBhZXRAZ21haWwuY29tIiwidXNlcklkIjoiNWFhYzViNTdmN2M3ZDY3OTdmZjUwZGI4IiwiaWF0IjoxNTIyMzE3NzQ4LCJleHAiOjE1MjIzMjEzNDh9.5lTw2w0rDqN1FKV81a2evE8yibeYR9AYjlXI3HrL_Fw',
+                'products'
+            );
             console.log('result: ', result);
+            fetchProducts(result.list);
         } catch (error) {
             console.log('error: ', error);
         }
     }
 
+    getColums() {
+        return [
+            {
+                Header: 'Description',
+                accessor: 'Description',
+                Cell: props => (
+                    <div className="text-center">
+                        {props.value}
+                    </div>
+                )
+            },
+            {
+                Header: 'Base Price',
+                accessor: 'BasePrice',
+                Cell: props => (
+                    <div className="text-center">
+                        {props.value}
+                    </div>
+                )
+            },
+            {
+                Header: 'Selling Price',
+                accessor: 'SellingPrice',
+                Cell: props => (
+                    <div className="text-center">
+                        {props.value}
+                    </div>
+                )
+            },
+            {
+                Header: 'Quantity',
+                accessor: d => d.Inventory.Quantity,
+                id: 'Quantity',
+                Cell: props => (
+                    <div className="text-center">
+                        {props.value}
+                    </div>
+                )
+            },
+            {
+                Header: 'Warning Quantity',
+                accessor: d => d.Inventory.WarningQuantity,
+                id: 'WarningQuantity',
+                Cell: props => (
+                    <div className="text-center">
+                        {props.value}
+                    </div>
+                )
+            },
+            {
+                Header: 'Actions',
+                Filter: () => {}
+            }
+        ];
+    }
+
     render() {
-        return <Layout title="Products" />;
+        let { data } = this.props;
+        console.log('data: ', data);
+        let columns = this.getColums();
+
+        return (
+            <Layout title="Products">
+                <ReactTable 
+                    filterable 
+                    data={data} 
+                    columns={columns}
+                    defaultPageSize={10}
+                />
+            </Layout>
+        );
     }
 }
 
