ddc0c9f63e2b70a6980c1cee705aab715eca89e2
Gabriel Joshua Paet <gjvpaet@gmail.com>
2018-03-29 19:18:42 +0800

    Added FAB button.

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -2,6 +2,7 @@ import { values } from 'lodash';
 import ReactTable from 'react-table';
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
+import { Menu, MainButton, ChildButton } from 'react-mfb';
 
 import actions from '../../store/actions';
 
@@ -27,7 +28,7 @@ class Products extends Component {
 
         try {
             let result = await httpService.getAllData(
-                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdqdnBhZXRAZ21haWwuY29tIiwidXNlcklkIjoiNWFhYzViNTdmN2M3ZDY3OTdmZjUwZGI4IiwiaWF0IjoxNTIyMzE3NzQ4LCJleHAiOjE1MjIzMjEzNDh9.5lTw2w0rDqN1FKV81a2evE8yibeYR9AYjlXI3HrL_Fw',
+                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdqdnBhZXRAZ21haWwuY29tIiwidXNlcklkIjoiNWFhYzViNTdmN2M3ZDY3OTdmZjUwZGI4IiwiaWF0IjoxNTIyMzIxNTc3LCJleHAiOjE1MjIzMjUxNzd9.rPzuT2LpKfSUU4WyOhbFsxqnSTgJBtXoHxdVNVxicrE',
                 'products'
             );
             console.log('result: ', result);
@@ -42,49 +43,29 @@ class Products extends Component {
             {
                 Header: 'Description',
                 accessor: 'Description',
-                Cell: props => (
-                    <div className="text-center">
-                        {props.value}
-                    </div>
-                )
+                Cell: props => <div className="text-center">{props.value}</div>
             },
             {
                 Header: 'Base Price',
                 accessor: 'BasePrice',
-                Cell: props => (
-                    <div className="text-center">
-                        {props.value}
-                    </div>
-                )
+                Cell: props => <div className="text-center">{props.value}</div>
             },
             {
                 Header: 'Selling Price',
                 accessor: 'SellingPrice',
-                Cell: props => (
-                    <div className="text-center">
-                        {props.value}
-                    </div>
-                )
+                Cell: props => <div className="text-center">{props.value}</div>
             },
             {
                 Header: 'Quantity',
                 accessor: d => d.Inventory.Quantity,
                 id: 'Quantity',
-                Cell: props => (
-                    <div className="text-center">
-                        {props.value}
-                    </div>
-                )
+                Cell: props => <div className="text-center">{props.value}</div>
             },
             {
                 Header: 'Warning Quantity',
                 accessor: d => d.Inventory.WarningQuantity,
                 id: 'WarningQuantity',
-                Cell: props => (
-                    <div className="text-center">
-                        {props.value}
-                    </div>
-                )
+                Cell: props => <div className="text-center">{props.value}</div>
             },
             {
                 Header: 'Actions',
@@ -100,12 +81,24 @@ class Products extends Component {
 
         return (
             <Layout title="Products">
-                <ReactTable 
-                    filterable 
-                    data={data} 
+                <ReactTable
+                    filterable
+                    data={data}
                     columns={columns}
                     defaultPageSize={10}
                 />
+                <Menu effect="slidein-spring" method="click" position="br">
+                    <MainButton
+                        style={{ color: 'white' }}
+                        iconActive="now-ui-icons ui-1_simple-remove"
+                        iconResting="now-ui-icons design_bullet-list-67"
+                    />
+                    <ChildButton
+                        label="Add Product"
+                        style={{ color: 'white' }}
+                        icon="now-ui-icons ui-1_simple-add"
+                    />
+                </Menu>
             </Layout>
         );
     }
