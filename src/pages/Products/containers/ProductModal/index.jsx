395130b30d8fa64be2c56b8affc8ba2659e140e6
Gabriel Joshua Paet <gjvpaet@gmail.com>
2018-03-30 19:10:23 +0800

    Installed react select.

--- src/pages/Products/containers/ProductModal/index.jsx
+++ src/pages/Products/containers/ProductModal/index.jsx
@@ -1,3 +1,4 @@
+import { connect } from 'react-redux';
 import React, { Component } from 'react';
 
 import Modal from '../../../../components/Modal/index.jsx';
--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -31,7 +31,7 @@ class Products extends Component {
 
         try {
             let result = await httpService.getAllData(
-                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdqdnBhZXRAZ21haWwuY29tIiwidXNlcklkIjoiNWFhYzViNTdmN2M3ZDY3OTdmZjUwZGI4IiwiaWF0IjoxNTIyMzg2Mzc5LCJleHAiOjE1MjIzODk5Nzl9.rN4lHXGK-vzCJrj45TX39lbsVau2qQqRRfdlzxJYJ-A',
+                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdqdnBhZXRAZ21haWwuY29tIiwidXNlcklkIjoiNWFhYzViNTdmN2M3ZDY3OTdmZjUwZGI4IiwiaWF0IjoxNTIyNDA3NTU0LCJleHAiOjE1MjI0MTExNTR9.nYxZmoAuSP0Zo4QzyVfvoixapx_Kjv8sZ810ggL2E2E',
                 'products'
             );
 
